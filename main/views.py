628cf516d29e26755e7af36ec8e65b8ec3104fdd
YazinAlhamdi <yazin.alhamdi@gmail.com>
2020-12-03 13:09:52 +0200

    New Version
    Add login, logout and items pages

--- main/views.py
+++ main/views.py
@@ -1,10 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from main.models import Item
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def homepage (request):
     return render(request, template_name = 'main/home.html')
 
-def itemspage(request):    
-    items = Item.objects.all()
-    return render(request, template_name = 'main/items.html', context= {'items' : items})
+def itemspage(request): 
+    if request.method == 'GET':   
+        items = Item.objects.all()
+        return render(request, template_name = 'main/items.html', context= {'items' : items})
+    if request.method == 'POST':
+        pass
+
+
+
+def loginpage(request):
+    if request.method == 'GET':
+        return render(request, template_name='main/login.html')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('items')
+        else:
+            return redirect('login')
+
+def registerpage(request):
+    if request.method == 'GET':
+        return render (request, template_name = 'main/register.html')
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+        else:
+            return redirect('register')
+
+def logoutpage (requset):
+    logout(requset)
+    return redirect ('home')
