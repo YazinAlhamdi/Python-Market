4695dd669698c6522730045677f8aaba830622f7
YazinAlhamdi <yazin.alhamdi@gmail.com>
2020-12-04 16:12:16 +0200

    Final version
    - Added purchase method

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from main.models import Item
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 # Create your views here.
 
 def homepage (request):
@@ -9,12 +10,16 @@ def homepage (request):
 
 def itemspage(request): 
     if request.method == 'GET':   
-        items = Item.objects.all()
+        items = Item.objects.filter(owner=None)
         return render(request, template_name = 'main/items.html', context= {'items' : items})
     if request.method == 'POST':
-        pass
-
-
+        purchased_item = request.POST.get('purchased-item')
+        if purchased_item:
+            purchased_item_object = Item.objects.get(name=purchased_item)
+            purchased_item_object.owner = request.user
+            purchased_item_object.save()
+            messages.success(request, f'Congratulations. You just bought {purchased_item_object.name} for {purchased_item_object.price}')
+        return redirect('items')
 
 def loginpage(request):
     if request.method == 'GET':
@@ -26,8 +31,10 @@ def loginpage(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request, f'You are logged in as {user.username}')
             return redirect('items')
         else:
+            messages.error(request, 'The combination of the user name and the password is wrong!')
             return redirect('login')
 
 def registerpage(request):
@@ -42,10 +49,13 @@ def registerpage(request):
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
             login(request, user)
+            messages.success(request, f'You have registered your account successfully! Logged in as {user.username}')
             return redirect('home')
         else:
+            messages.error(request, form.errors)
             return redirect('register')
 
-def logoutpage (requset):
-    logout(requset)
-    return redirect ('home')
+def logoutpage(request):
+    logout(request)
+    messages.success(request, f'You have been logged out!')
+    return redirect('home')
